bf1f9b83a539d548f690f3c75b03cc972bfa507c
Brunno <brunno.oliveira.nextage@gmail.com>
2023-09-04 20:28:06 -0300

    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sistema Mentor</h1>
+    <div>
+        <h2>
+            Faça seu login:
+        </h2>
+        <form action="#" method="POST">
+            <div>
+                <label for="cadastro-login">
+                    E-mail:
+                </label>
+                <input type="text">
+            </div>
+            <div>
+                <label for="cadastro-login">
+                    Senha:
+                </label>
+                <input type="text">
+            </div>
+        </form>
+    </div>
+
+    <div>
+        <h2>
+            Faça seu cadastro
+        </h2>
+        <form action="#" method="POST">
+            <div>
+                <label for="cadastro-login">
+                    Cadastro:
+                </label>
+                <input type="text">
+            </div>
+
+            <div>
+                <label for="cadastro-email">
+                    E-mail:
+                </label>
+                <input type="text">
+            </div>
+
+            <div>
+                <label for="cadastro-senha">
+                    Senha:
+                </label>
+                <input type="password">
+            </div>
+
+            <div>
+                <label for="cadastro-senha-confirmacao">
+                    Confirmar senha:
+                </label>
+                <input type="password">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
